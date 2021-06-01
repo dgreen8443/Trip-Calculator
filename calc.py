7a57a4ebbd97a0f7c5fff193e72b45fcf429e7b7
David Green <44809262+dgreen8443@users.noreply.github.com>
2021-06-01 15:26:56 +0100

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,105 @@
+import re
+import numpy as np
+import json 
+
+payments_Text = "./payments.JSON"
+people_Text = "./people.txt"
+
+
+
+def readPeopleIn():
+    with open(people_Text) as f:
+        content = f.readlines()
+    temp = []
+    for line in content:
+        temp.append(line)
+    new_items = [x[:-1] for x in temp]
+    return new_items
+
+def readPaymentsIn():
+    with open('payments.JSON', 'r') as myfile:
+        data = myfile.read()
+    output = json.loads(data)
+    return output
+        
+def addShitUp(people, payments):
+    tallies = {}
+    for i in range(len(people)):
+        name = people[i]
+        tallies[name] = 0
+
+    for pay in payments:
+        extract = payments[pay]
+        tallies[extract[0]] += extract[1]
+    
+    return tallies
+
+
+def mathShit(tallies, people, payments):
+    totalCost = 0
+    returnDict = {}
+    for ele in tallies:
+        totalCost += tallies[ele]
+    even = totalCost / len(people)
+    for person in tallies:
+        if tallies[person] < even:
+            owes = even - tallies[person]
+            print(person + " owes " + (str)(owes))
+            returnDict[person] = owes
+        else:
+            returnDict[person] = even - tallies[person]
+
+    return returnDict
+
+def findPayer(owings):
+    for person in owings:
+        if owings[person] > 0:
+            return person
+def findPayee(owings):
+    for person in owings:
+        if owings[person] < 0:
+            return person
+
+
+def balanceItOut(owings):
+    q = True
+    while (q):
+        payer = findPayer(owings)
+        payee = findPayee(owings)
+        if (owings[payee] * (-1)) >= owings[payer]:
+            print(payer + " pays " + (str)(owings[payer]) + " to " + payee)
+            temp = owings[payer]
+            owings[payer] -= owings[payer]
+            owings[payee] += temp
+        else:
+            print(payer + " pays " + (str)(owings[payee]* (-1)) + " to " + payee)
+            temp = owings[payee]
+            owings[payee] -= owings[payee]
+            owings[payer] += temp
+        bal = 0
+        for person in owings:
+            if owings[person] != 0:
+                break
+            else:
+                bal += 1
+        if (bal == len(owings)):
+            q = False
+        
+
+        
+
+
+def main():
+    people = readPeopleIn()
+    payments = readPaymentsIn()
+    #print(people)
+    #print(payments)
+    tallies = addShitUp(people, payments)
+    #print(tallies)
+    owings = mathShit(tallies, people, payments)
+    #print(owings)
+    balanceItOut(owings)
+    #print(owings)
+
+if __name__ == "__main__":
+    main()
